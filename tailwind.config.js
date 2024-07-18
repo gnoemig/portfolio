1869c89088ed892dddf85f458a498649fa2cc8be
gnoemig <110119979+gnoemig@users.noreply.github.com>
2024-07-18 09:55:49 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
